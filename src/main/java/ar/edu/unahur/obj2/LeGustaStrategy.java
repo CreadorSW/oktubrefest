5f1c1e0975b7c4668096d1124a04f2c380020e42
CreadorSW <17933209+CreadorSW@users.noreply.github.com>
2026-05-06 13:46:29 -0300

    avanzar en el modelado de strategy leGusta

---
+++ src/main/java/ar/edu/unahur/obj2/LeGustaStrategy.java
@@ -0,0 +1,8 @@
+package ar.edu.unahur.obj2;
+
+import ar.edu.unahur.obj2.marcas.Marca;
+
+public interface LeGustaStrategy {
+    // En Java los métodos de una interfaz son abstractos por defecto.
+    boolean leGusta(Marca marca);
+}
--- src/main/java/ar/edu/unahur/obj2/Persona.java
+++ src/main/java/ar/edu/unahur/obj2/Persona.java
@@ -1,5 +1,6 @@
 package ar.edu.unahur.obj2;
 
+import ar.edu.unahur.obj2.marcas.Marca;
 import java.util.List;
 
 public class Persona {
@@ -8,6 +9,8 @@ public class Persona {
     private List<Jarra> jarras;
     private Boolean escuchaMusicaTradicional;
     private Double nivelDeAguante;
+    // leGustaStrategy es una variable que va a ser una estrategia diferente según la persona.
+    private LeGustaStrategy leGustaStrategy;
 
     public Persona(
         Double peso,
@@ -41,7 +44,14 @@ public class Persona {
         return cantidadDeAlcoholIngerido() * this.peso > this.nivelDeAguante;
     }
 
+    // refactor. siempre privado.
+    // Si lo pusiese en público, estoy cambiando el qué. Doy una posibilidad más de que alguien me mensajee. Un refactor siempre es mejor tenerlo privado. Nadie tiene que saber cosas de más.
     private Double cantidadDeAlcoholIngerido() {
         return jarras.stream().mapToDouble(Jarra::getContenido).sum();
+    }
+
+    // La persona recibe el mensaje leGusta(marca) y se lo reenvía a la estrategia. La persona no sabe la respuesta. Pero sabe que **tiene** un objeto (leGustaStrategy) que sí sabe responder esa pregunta.
+    public Boolean leGusta(Marca marca) {
+        return leGustaStrategy.leGusta(marca);
     }
 }
