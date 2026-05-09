12dd26afeb71fa480227443b6d2c209cf25b2567
CreadorSW <17933209+CreadorSW@users.noreply.github.com>
2026-05-09 10:50:05 -0300

    corregir método y atributo capacidad en Jarra

--- src/main/java/ar/edu/unahur/obj2/Jarra.java
+++ src/main/java/ar/edu/unahur/obj2/Jarra.java
@@ -5,18 +5,18 @@ import ar.edu.unahur.obj2.marcas.Marca;
 public class Jarra {
 
     private Marca marca;
-    private Double contenido;
+    private Double capacidad;
 
-    public Jarra(Marca marca, Double contenido) {
+    public Jarra(Marca marca, Double capacidad) {
         this.marca = marca;
-        this.contenido = contenido;
+        this.capacidad = capacidad;
     }
 
     public Marca getMarca() {
         return marca;
     }
 
-    public Double getContenido() {
-        return contenido;
+    public Double getCapacidad() {
+        return capacidad;
     }
 }
--- src/main/java/ar/edu/unahur/obj2/Persona.java
+++ src/main/java/ar/edu/unahur/obj2/Persona.java
@@ -47,7 +47,7 @@ public class Persona {
     // refactor. siempre privado.
     // Si lo pusiese en público, estoy cambiando el qué. Doy una posibilidad más de que alguien me mensajee. Un refactor siempre es mejor tenerlo privado. Nadie tiene que saber cosas de más.
     private Double cantidadDeAlcoholIngerido() {
-        return jarras.stream().mapToDouble(Jarra::getContenido).sum();
+        return jarras.stream().mapToDouble(Jarra::getCapacidad).sum();
     }
 
     // La persona recibe el mensaje leGusta(marca) y se lo reenvía a la estrategia. La persona no sabe la respuesta. Pero sabe que **tiene** un objeto (leGustaStrategy) que sí sabe responder esa pregunta.
