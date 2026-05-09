95d7adeb690663f18ea1bf92811db9f401a61f68
CreadorSW <17933209+CreadorSW@users.noreply.github.com>
2026-05-09 11:34:01 -0300

    hacer public cantidadDeAlcoholIngerido, corregir método

--- src/main/java/ar/edu/unahur/obj2/Persona.java
+++ src/main/java/ar/edu/unahur/obj2/Persona.java
@@ -51,8 +51,12 @@ public class Persona {
 
     // refactor. siempre privado.
     // Si lo pusiese en público, estoy cambiando el qué. Doy una posibilidad más de que alguien me mensajee. Un refactor siempre es mejor tenerlo privado. Nadie tiene que saber cosas de más.
-    private Double cantidadDeAlcoholIngerido() {
-        return jarras.stream().mapToDouble(Jarra::getCapacidadLitros).sum();
+    // PERO después lo cambié a public porque era algo que pedía el test...
+    public Double cantidadDeAlcoholIngerido() {
+        return jarras
+            .stream()
+            .mapToDouble(Jarra::contenidoDeAlcoholLitros)
+            .sum();
     }
 
     // La persona recibe el mensaje leGusta(marca) y se lo reenvía a la estrategia. La persona no sabe la respuesta. Pero sabe que **tiene** un objeto (leGustaStrategy) que sí sabe responder esa pregunta.
